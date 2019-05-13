07a17c77ab3157d1a864847562c90fd54f4e820b
Justin Shenk <shenkjustin@gmail.com>
2019-05-13 16:17:45 +0200

    Update with Options UI

--- background.js
+++ background.js
@@ -1,4 +1,66 @@
-// Copyright (c) 2019 Justin Shenk.
+chrome.runtime.onInstalled.addListener(d => {
+    chrome.storage.sync.get(["acceptedRisks", "settings", "pluginSettings"], res => {
+        if (!res.acceptedRisks) {
+            chrome.tabs.create({ url: chrome.extension.getURL("options.html") + "?fullpage=1" });
+        }
+
+				if (!res.pluginSettings) {
+					let pluginSettings = {};
+					if (res.settings) {
+							// it needs an update!
+							const settings = JSON.parse(res.settings);
+							pluginSettings = {
+                    agree_all: {
+											enabled: true,
+                    },
+									};
+							} else {
+								// it"s a fresh install, load the defaults
+								pluginSettings = {
+									agree_all: {
+                        enabled: true
+                    },
+									};
+					}
+					chrome.storage.sync.set({
+							pluginSettings: JSON.stringify(pluginSettings)
+					});
+				} else {
+	          let pluginUpdated = false;
+	          const pluginSettings = JSON.parse(res.pluginSettings);
+
+	          // new plugins
+	          if (pluginSettings.agree_all === undefined) {
+	              pluginUpdated = true;
+	              pluginSettings.agree_all = { enabled: true };
+	          }
+
+	          if (pluginUpdated) {
+	              chrome.storage.sync.set({
+	                  pluginSettings: JSON.stringify(pluginSettings)
+	              });
+	          }
+        }
+    });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type === "slackPageOpened") {
+        chrome.pageAction.show(sender.tab.id);
+    } else if (request.type === "closeThisTab") {
+        chrome.tabs.remove(sender.tab.id);
+    // } else if (request.type.startsWith("Agreed")) {
+    //     const parts = request.type.split(".");
+    //     const potentialClass = availablePlugins[parts[1]];
+		//
+    //     if (potentialClass) {
+    //         request.type = parts[2];
+    //         potentialClass.ProcessExtensionMessage(request, parts[1], sender);
+    //     }
+    }
+});
+
+
 chrome.commands.onCommand.addListener(function(command) {
   if (command === "agree-first-reaction") {
     chrome.tabs.executeScript({
@@ -34,5 +96,4 @@ chrome.commands.onCommand.addListener(function(command) {
       clickEveryReaction()`
     })
   }
-
 });
