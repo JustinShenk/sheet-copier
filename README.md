380e570040340a50ef1cdbd70a139f9857a9db60
Justin Shenk <shenkjustin@gmail.com>
2019-05-13 10:24:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Agreed - Slack Agree Hotkey :thumbsup:
+
+Slack "Agree" shortcut. Press `Ctrl/Cmd+Shift+Y` to agree with first reaction to every post, press `Ctrl/Cmd+Shift+X` to agree with every reaction.
+
+Before -> After
+---------------
+
+![before](before)
+
+![after](after)
+
+
+Hotkeys
+-------
+
+* `Ctrl/Command + Shift + Y` - Agree with first
+* `Ctrl/Command + Shift + X` - Agree with all
---
+++ background.js
@@ -0,0 +1,45 @@
+// Copyright (c) 2019 Justin Shenk.
+chrome.browserAction.onClicked.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.id, {
+		file: 'inject.js'
+	});
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  if (command === "agree-first-reaction") {
+    chrome.tabs.executeScript({
+      code: `
+      function clickFirstReaction() {
+          const query = "div.c-reaction_bar > button:nth-child(1):not(.c-reaction--reacted)";
+          const reactionsToClick = document.querySelectorAll(query);
+          const beforeCount = reactionsToClick.length;
+
+          if (reactionsToClick.length) {
+              reactionsToClick.forEach((button) => { button.click()} );
+          }
+
+          const afterCount = document.querySelectorAll(query).length;
+      }
+      clickFirstReaction()`
+    })
+  }
+  else if (command === "agree-every-reaction") {
+    chrome.tabs.executeScript({
+      code: `
+      function clickEveryReaction() {
+        const query = "div > button.c-button-unstyled.c-reaction:not(.c-reaction--reacted) > span.emoji-outer.emoji-sizer";
+        const reactionsToClick = document.querySelectorAll(query);
+        const beforeCount = reactionsToClick.length;
+
+        if (reactionsToClick.length) {
+            reactionsToClick.forEach((button) => { button.click()} );
+        }
+
+        const afterCount = document.querySelectorAll(query).length;
+      }
+      clickEveryReaction()`
+    })
+  }
+
+});
