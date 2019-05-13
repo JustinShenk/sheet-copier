8f0f4a6df95af01cfbb371bbcd3608da93b3560c
Justin Shenk <shenkjustin@gmail.com>
2019-05-13 11:37:06 +0200

    Add tooltip

--- background.js
+++ background.js
@@ -1,11 +1,4 @@
 // Copyright (c) 2019 Justin Shenk.
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.id, {
-		file: 'inject.js'
-	});
-});
-
 chrome.commands.onCommand.addListener(function(command) {
   if (command === "agree-first-reaction") {
     chrome.tabs.executeScript({
